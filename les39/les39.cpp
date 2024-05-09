9f42587f2c0cca05a4d929a2f38e2cc6d34a580d
Артуриус <@DESKTOP>
2024-05-09 20:23:03 +0300

    Добавьте файлы проекта.

---
+++ les39/les39.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int hapy(unsigned int num)
+{
+    return ((num / 100000) + ((num / 10000) % 10) + ((num / 1000) % 10)) == (((num / 100) % 10) + ((num / 10) % 10) + (num % 10));
+}
+
+int main()
+{
+    int out = hapy(135432);
+    if (out == 1)
+        cout << "happy" << endl;
+     if (out == 0)
+        cout << "not happy" << endl;
+}
